d491db381effc7ddb1a889eb898a7851411eca2a
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-01 04:57:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,9 +34,9 @@ def select_series_title_with_most_human_characters
   INNER JOIN books
   ON series.id = book.series_id
   INNER JOIN books ON character_books.book_id = books.id
-  JOIN character ON character_books.character_id = character.id
+  JOIN characters ON character_books.character_id = character.id
   WHERE character.name = 'human'
-  GROUP BY series.title
+  GROUP BY series.title 
   HAVING COUNT(character_books.character_id) = (SELECT MAX(COUNT(character_books.character_id)))"
 end
 
