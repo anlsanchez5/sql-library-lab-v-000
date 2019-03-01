95b2465f2a62ebbf4989d38273e30815b2e2e74c
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-01 05:41:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,8 +34,8 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species = 'human'
-  GROUP BY series.title
-  HAVING COUNT(characters.species) = (SELECT MAX (COUNT(characters.species)))"
+  ORDER BY COUNT(character.series) 
+  DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
