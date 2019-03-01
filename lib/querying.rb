124073e8ed2b0f7f758b9323aee2edb8eab3e57f
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-01 05:31:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
-  INNER JOIN characters
+  JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species = 'human'"
 end
